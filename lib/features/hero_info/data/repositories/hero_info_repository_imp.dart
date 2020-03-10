72fa35cac9bf4d5a3b73a73a6ce79d2de5a619e9
Eric Maxwell <max.souza_@hotmail.com>
2020-03-10 18:01:49 -0300

    Refactoring hero_info_repository_imp to make it clean and elegant

--- lib/features/hero_info/data/repositories/hero_info_repository_imp.dart
+++ lib/features/hero_info/data/repositories/hero_info_repository_imp.dart
@@ -5,8 +5,12 @@ import 'package:tdd_studing/core/error/failures.dart';
 import 'package:tdd_studing/core/platform/network_info.dart';
 import 'package:tdd_studing/features/hero_info/data/datasources/hero_info_local_data_source.dart';
 import 'package:tdd_studing/features/hero_info/data/datasources/hero_info_remote_data_source.dart';
+import 'package:tdd_studing/features/hero_info/data/models/hero_model.dart';
 import 'package:tdd_studing/features/hero_info/domain/entities/hero.dart';
 import 'package:tdd_studing/features/hero_info/domain/repositories/hero_info_repository.dart';
+
+//define a type to concreteOrdandomChooser function of Future<HeroModel>
+typedef Future<HeroModel> _ConcreteOrRandomChooser();
 
 class HeroInfoRepositoryImpl implements HeroInfoRepository {
   final HeroInfoRemoteDataSource remoteDataSource;
@@ -21,30 +25,26 @@ class HeroInfoRepositoryImpl implements HeroInfoRepository {
 
   @override
   Future<Either<Failure, Hero>> getHero(String id) async {
-    if (await networkInfo.isConnected) {
-      try {
-        final remoteHero = await remoteDataSource.getHero(id);
-        localDataSource.cacheHero(remoteHero);
-        return Right(remoteHero);
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        final localHero = await localDataSource.getLastHero();
-        return Right(localHero);
-      } on CacheException {
-        return Left(CacheFailure());
-      }
-    }
+    return await _getTrivia(() {
+      //the method that will be used on remoteDataSource
+      return remoteDataSource.getHero(id);
+    });
   }
 
   @override
   Future<Either<Failure, Hero>> getRandomHero() async {
-    // TODO: implement listHeroes
+    return await _getTrivia(() {
+      //the method that will be used on remoteDataSource
+      return remoteDataSource.getRandomHero();
+    });
+  }
+
+  //recieve a function as parameter that contains what kind of method will be used
+  Future<Either<Failure, Hero>> _getTrivia(
+      _ConcreteOrRandomChooser getHeroInfoOrRandom) async {
     if (await networkInfo.isConnected) {
       try {
-        final remoteHero = await remoteDataSource.getRandomHero();
+        final remoteHero = await getHeroInfoOrRandom();
         localDataSource.cacheHero(remoteHero);
         return Right(remoteHero);
       } on ServerException {
