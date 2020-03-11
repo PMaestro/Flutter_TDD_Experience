dc3732b86a5e1c6f0fbc5dabca4d6a2f3ef91b1d
Eric Maxwell <max.souza_@hotmail.com>
2020-03-11 11:35:26 -0300

    adding test to getLastHeroInfo using sharedPreferences

--- lib/features/hero_info/data/datasources/hero_info_local_data_source.dart
+++ lib/features/hero_info/data/datasources/hero_info_local_data_source.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tdd_studing/features/hero_info/data/models/hero_model.dart';
 
 abstract class HeroInfoLocalDataSource {
@@ -7,4 +11,22 @@ abstract class HeroInfoLocalDataSource {
   /// Throws [NoLocalDataException] if no cached data is present.
   Future<HeroModel> getLastHero();
   Future<void> cacheHero(HeroModel heroToCache);
+}
+
+class HeroInfoLocalDataSourceImpl implements HeroInfoLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  HeroInfoLocalDataSourceImpl({@required this.sharedPreferences});
+
+  @override
+  Future<void> cacheHero(HeroModel heroToCache) {
+    // TODO: implement cacheHero
+    return null;
+  }
+
+  @override
+  Future<HeroModel> getLastHero() {
+    final jsonString = sharedPreferences.getString('CACHED_HERO_INFO');
+    return Future.value(HeroModel.fromJson(json.decode(jsonString)));
+  }
 }
---
+++ test/features/hero_info/data/datasources/hero_info_local_data_source_test.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tdd_studing/features/hero_info/data/datasources/hero_info_local_data_source.dart';
+import 'package:tdd_studing/features/hero_info/data/models/hero_model.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  HeroInfoLocalDataSourceImpl dataSource;
+  MockSharedPreferences mockSharedPreferences;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource =
+        HeroInfoLocalDataSourceImpl(sharedPreferences: mockSharedPreferences);
+  });
+
+  group('getLastHeroInfo', () {
+    final tHeroModel = HeroModel.fromJson(json.decode(fixture('hero.json')));
+    test(
+        'should return HeroInfo from SharedPreferences when there is one in the chache',
+        () async {
+      when(mockSharedPreferences.getString(any))
+          .thenReturn(fixture('hero.json'));
+
+      final result = await dataSource.getLastHero();
+
+      verify(mockSharedPreferences.getString('CACHED_HERO_INFO'));
+      expect(result, equals(tHeroModel));
+    });
+  });
+}
